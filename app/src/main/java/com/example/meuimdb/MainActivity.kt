d83474b882dcbf2db60f6b015d306646cf877ad9
RAFAEL CAMPERA DE LEAO <05163643088@senacrs.edu.br>
2025-10-24 19:18:31 -0300

    Alteração

--- app/src/main/java/com/example/meuimdb/MainActivity.kt
+++ app/src/main/java/com/example/meuimdb/MainActivity.kt
@@ -13,35 +13,3 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.meuimdb.ui.theme.MeuIMDBTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MeuIMDBTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MeuIMDBTheme {
-        Greeting("Android")
-    }
-}
